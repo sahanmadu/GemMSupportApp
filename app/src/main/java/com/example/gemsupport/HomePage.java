26a3aa25e10b3af06bade8430cd3bc89051a4f86
sahanmadu <sahanmadusanka640@gmail.com>
2021-01-13 18:47:31 +0530

    get user account values from firebase

--- app/src/main/java/com/example/gemsupport/HomePage.java
+++ app/src/main/java/com/example/gemsupport/HomePage.java
@@ -5,15 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomePage extends AppCompatActivity {
-
+    Button btnUAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        btnUAccount=findViewById(R.id.btnUAccount);
+
+
+
+        btnUAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), UserAccount.class));
+            }
+        });
+
     }
 
     public void logout(View view) {
--- app/src/main/java/com/example/gemsupport/UserAccount.java
+++ app/src/main/java/com/example/gemsupport/UserAccount.java
@@ -1,14 +1,44 @@
 package com.example.gemsupport;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class UserAccount extends AppCompatActivity {
+    TextView fname,email,pno;
+    FirebaseAuth fauth;
+    FirebaseFirestore fstore;
+    String uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_account);
+        fname=findViewById(R.id.txtFname);
+        email=findViewById(R.id.txtEmails);
+        pno=findViewById(R.id.txtNo);
+        fauth=FirebaseAuth.getInstance();
+        fstore=FirebaseFirestore.getInstance();
+
+        uid=fauth.getCurrentUser().getUid();
+
+        DocumentReference documentReference=fstore.collection("users").document(uid);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                fname.setText(value.getString("Fullname"));
+                email.setText(value.getString("Email"));
+                pno.setText(value.getString("PhoneNumber"));
+            }
+        });
     }
 }
